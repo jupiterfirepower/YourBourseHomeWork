a2d33bea2a7d8f2d7a3c4a83b62a96ecc84fe637
Oleh Parshenko <jupiterfiretetraedr@gmail.com>
2022-08-23 20:01:19 +0300

    remove unneeded

--- client/src/references/codegen/models/WebError.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type WebError = {
-    errorCode?: number;
-    header?: string | null;
-    message?: string | null;
-};
--- client/src/references/codegen/models/WebErrorResult.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-import type { WebError } from './WebError';
-
-export type WebErrorResult = {
-    errors?: Array<WebError> | null;
-};
